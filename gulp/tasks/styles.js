050da0c74d0940d5fa1a5e555cd9697d04481e08
Daryl Lozupone <daryl@clubdeuce.net>
2019-03-01 12:46:15 -0500

    Implemented basic header styling

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,14 @@ autoprefixer = require('autoprefixer'),
 variables = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
-mixins = require('postcss-mixins');
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
+
+let plugins = [cssImport, mixins, variables, nested, hexrgba, autoprefixer];
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postCss([cssImport,  mixins, variables, nested, autoprefixer]))
+    .pipe(postCss(plugins))
     .on('error', function(info){
       console.log(info.toString());
       this.emit('end');
